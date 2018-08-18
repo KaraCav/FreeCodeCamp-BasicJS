a0137473a0c4087dd3c9842a4d8f7a257f6afb57
Kara Cavanaugh <karacav@msn.com>
2018-08-18 14:05:34 -0400

    Number 1

---
+++ Part1.js
@@ -0,0 +1,9 @@
+function multiplyAll(arr) {
+  var product = 1;
+  for(var i=0; i < arr.length; i++) {
+  for (var j=0; j < arr[i].length; j++) {
+product = product * arr[i][j];
+  }
+}
+  return product;
+}
