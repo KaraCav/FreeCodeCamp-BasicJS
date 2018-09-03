4e83cf0ce9120fdf4dfb43009f940db993aab9d8
Kara Cavanaugh <karacav@msn.com>
2018-09-03 14:03:12 -0400

    Number 2

--- Part1.js
+++ Part1.js
@@ -1,3 +1,4 @@
+// 1. For Loops
 function multiplyAll(arr) {
   var product = 1;
   for(var i=0; i < arr.length; i++) {
@@ -7,3 +8,17 @@ product = product * arr[i][j];
 }
   return product;
 }
+// 2. Iterate with JavaScript Do...While Loops
+var myArray = [];
+var i = 10;
+do{
+   myArray.push(i);
+  i++;
+}
+while (i < 5);
+
+// 3. Profile Lookup: We have an array of objects representing different people in our contacts lists. 
+// A lookUpProfile function that takes name and a prop as arguments has been pre-written for you. 
+// It should check if name is an actual contact's firstName and the given property (prop) is a property of that contact.
+// If both are true, return the "value" of the property. If it doesn't correspond to any contacts return "No such contact"
+// If prop does not correspond to any valid properties of a contact found to match name then return "No such property"
